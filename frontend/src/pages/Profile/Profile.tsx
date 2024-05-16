af0192abf7ba74c73481aa6a9ebad76210170841
JeanMichelBB <jeanmichelbberube@outlook.com>
2024-05-16 12:39:37 -0400

    Update Profile

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -4,6 +4,7 @@ import NotFound from '../NotFound/NotFound';
 import User from '../../UserModel';
 import Connection from '../../components/Connection/Connection';
 import ConnectionButton from '../../components/ConnectionButton/ConnectionButton';
+import { Link } from 'react-router-dom';
 
 export interface UserData {
     id: string;
@@ -31,16 +32,6 @@ type ProfileData = UserData | ErrorData | null;
 const Profile = ({ logUsername, user }: ProfileProps) => {
     const { username } = useParams();
     const [userData, setUserData] = useState<ProfileData>(null);
-    const [editing, setEditing] = useState(false);
-    const [formData, setFormData] = useState<UserData>({
-        id: '',
-        username: '',
-        full_name: '',
-        bio: '',
-        location: '',
-        website: '',
-        profile_image: '',
-    });
     const [loading, setLoading] = useState(true);
     useEffect(() => {
         const fetchProfileData = async () => {
@@ -50,7 +41,6 @@ const Profile = ({ logUsername, user }: ProfileProps) => {
                     const data = await response.json();
                     setUserData(data);
                     console.log('Profile data:', data);
-                    setFormData(data as UserData);
                 } else {
                     // Redirect to 404 page if user not found
                     setUserData({ error: 'User not found' });
@@ -68,49 +58,6 @@ const Profile = ({ logUsername, user }: ProfileProps) => {
 
     const isCurrentUser = logUsername === username;
 
-    const handleEdit = () => {
-        setEditing(true);
-    };
-
-    const handleCancel = () => {
-        setEditing(false);
-        setFormData(userData as UserData);
-    };
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
-
-    const handleSubmit = async () => {
-        try {
-            const { full_name, bio, location, website, profile_image } = formData;
-            const url = `http://127.0.0.1:8000/profile/${username}?full_name=${full_name}&bio=${bio}&location=${location}&website=${website}&profile_picture=${profile_image}`;    
-            const token = localStorage.getItem('token');
-            // Send updated profile data to backend
-            const response = await fetch(url, {
-                method: 'PUT',
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-            if (response.ok) {
-                // Handle success, e.g., show success message
-                console.log('Profile updated successfully');
-                setEditing(false);
-                setUserData(formData);
-            } else {
-                // Handle error response, e.g., show error message
-                console.error('Profile update failed');
-            }
-        } catch (error) {
-            console.error('Error updating profile:', error);
-            // Handle fetch error
-        }
-    };
 
     if (loading) {
         return <div>Loading...</div>;
@@ -124,9 +71,9 @@ const Profile = ({ logUsername, user }: ProfileProps) => {
         <div>
             <h2>Profile Page</h2>
             {userData && <ConnectionButton currentUser={user} visitedUser={userData} />}
-            {userData && <Connection currentUser={user} visitedUser={userData}  />}
+            {userData && <Connection currentUser={user} visitedUser={userData} />}
             {/* Render profile data */}
-            {userData && !editing && (
+            {userData && (
                 <div>
                     <p>Username: {userData.username}</p>
                     <p>Full Name: {userData.full_name}</p>
@@ -135,33 +82,10 @@ const Profile = ({ logUsername, user }: ProfileProps) => {
                     <p>Website: {userData.website}</p>
                     <p>Profile Image: {userData.profile_image}</p>
                     {/* Add other profile fields */}
-                    {isCurrentUser && <button onClick={handleEdit}>Edit Profile</button>}
-                </div>
-            )}
-
-            {/* Render input fields when editing */}
-            {editing && (
-                <div>
-                    <label htmlFor="username">Username:</label>
-                    <input type="text" name="username" value={formData?.username || ''} onChange={handleChange} />
-                    <br />
-                    <label htmlFor="full_name">Full Name:</label>
-                    <input type="text" name="full_name" value={formData?.full_name || ''} onChange={handleChange} />
-                    <br />
-                    <label htmlFor="bio">Bio:</label>
-                    <input type="text" name="bio" value={formData?.bio || ''} onChange={handleChange} />
-                    <br />
-                    <label htmlFor="location">Location:</label>
-                    <input type="text" name="location" value={formData?.location || ''} onChange={handleChange} />
-                    <br />
-                    <label htmlFor="website">Website:</label>
-                    <input type="text" name="website" value={formData?.website || ''} onChange={handleChange} />
-                    <br />
-                    <label htmlFor="profile_image">Profile Image:</label>
-                    <input type="text" name="profile_image" value={formData?.profile_image || ''} onChange={handleChange} />
-                    <br />
-                    <button onClick={handleCancel}>Cancel</button>
-                    <button onClick={handleSubmit}>Save</button>
+                    {isCurrentUser && (
+                        // need to edit got to setting
+                        <Link to="/settings"> Edit Profile</Link>
+                    )}                
                 </div>
             )}
         </div>
