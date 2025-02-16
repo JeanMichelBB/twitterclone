03f145ec5a888d0de7b8e595581c114977c9aa9c
JeanMichelBB <jeanmichelbberube@outlook.com>
2025-02-16 12:59:50 -0400

    update vite react secret

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,2 @@
-// export const apiKey = import.meta.env.VITE_APP_API_KEY;
-// export const apiUrl = import.meta.env.VITE_APP_API_URL;
-
-export const apiKey = process.env.VITE_APP_API_KEY;
-export const apiUrl = process.env.VITE_APP_API_URL;
+export const apiKey = import.meta.env.VITE_APP_API_KEY;
+export const apiUrl = import.meta.env.VITE_APP_API_URL;
