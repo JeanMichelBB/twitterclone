29765c807dd4ee86d18337f79c8fda5c3730cadb
JeanMichelBB <jeanmichelbberube@outlook.com>
2025-02-16 10:57:02 -0400

    create env.js for secret

---
+++ frontend/public/env.js
@@ -0,0 +1,5 @@
+// public/env.js
+window.__ENV__ = {
+    VITE_APP_API_KEY: "",
+    VITE_APP_API_URL: ""
+  };
--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,2 +1,16 @@
-export const apiKey = import.meta.env.VITE_APP_API_KEY;
-export const apiUrl = import.meta.env.VITE_APP_API_URL;
+// export const apiKey = import.meta.env.VITE_APP_API_KEY;
+// export const apiUrl = import.meta.env.VITE_APP_API_URL;
+
+export {};
+
+declare global {
+  interface Window {
+    __ENV__: {
+      VITE_APP_API_KEY: string;
+      VITE_APP_API_URL: string;
+    };
+  }
+}
+
+export const apiKey = window.__ENV__.VITE_APP_API_KEY;
+export const apiUrl = window.__ENV__.VITE_APP_API_URL;
