0dbfd0a1b152805f11734dfd05ed8855ea715c82
JeanMichelBB <jeanmichelbberube@outlook.com>
2025-02-16 12:46:39 -0400

    update .env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// Update your Vite config
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,5 +11,10 @@ export default defineConfig({
     chunkSizeWarningLimit: 10000,
     outDir: 'dist', // This specifies the output directory
     sourcemap: false,
+  },
+  define: {
+    // If you want to ensure specific env variables are exposed (usually not necessary for VITE_ prefixed variables)
+    'process.env.VITE_APP_API_KEY': JSON.stringify(process.env.VITE_APP_API_KEY),
+    'process.env.VITE_APP_API_URL': JSON.stringify(process.env.VITE_APP_API_URL),
   }
 })
